bd3d04924fb37bfece1146d8c5c7d64e8955fbe3
Nick Schorr <naschorr@gmail.com>
2021-06-28 14:59:06 -0400

    Updated fidelity userscript for improved reliability and matching

--- fidelity-landing-page-tweaker.js
+++ fidelity-landing-page-tweaker.js
@@ -2,9 +2,10 @@
 // @name         Fidelity Landing Page Tweaker
 // @namespace    https://github.com/naschorr/userscripts
 // @version      0.1
-// @description  Tweaks the "Today's Markets" graph to show your desired fund & date range
+// @description  Tweaks the "Today's Markets" graph to show your desired market & date range
 // @author       naschorr
 // @match        https://www.fidelity.com/
+// @match        https://www.fidelity.com/?*
 // @icon         https://www.google.com/s2/favicons?domain=fidelity.com
 // @grant        none
 // ==/UserScript==
@@ -12,15 +13,15 @@
 (function() {
     'use strict';
 
-    // Uncomment the index that you'd like to be plotted:
-    // const INDEX_NAME = 'dji';    // DJIA (Default)
-    // const INDEX_NAME = 'nasdaq'; // NASDAQ
-    const INDEX_NAME = 'spx';       // S&P 500
-    // const INDEX_NAME = 'ftse';   // FTSE 100
-    // const INDEX_NAME = 'nikkei'; // NIKKEI 225
+    // Uncomment the market that you'd like to be plotted:
+    // const MARKET_NAME = 'dji';    // DJIA (Default)
+    // const MARKET_NAME = 'nasdaq'; // NASDAQ
+    const MARKET_NAME = 'spx';       // S&P 500
+    // const MARKET_NAME = 'ftse';   // FTSE 100
+    // const MARKET_NAME = 'nikkei'; // NIKKEI 225
 
     // Uncomment the date range that you'd like to be plotted:
-    const DATE_RANGE = '1D'; // One day (Default)
+    const DATE_RANGE = '1D';    // One day (Default)
     // const DATE_RANGE = '5D'; // Five days
     // const DATE_RANGE = '1M'; // One month
     // const DATE_RANGE = '6M'; // Six months
@@ -29,11 +30,17 @@
     window.addEventListener('load', () => {
         // Force into next render cycle, because sometimes the graph still isn't ready after it's fully loaded
         setTimeout(() => {
-            // Select the index
-            document.querySelector(`div.market-chart--container div[id*='${INDEX_NAME}']`).click();
+            // Select the market
+            const marketSelector = document.querySelector(`div.market-chart--container div[id*='${MARKET_NAME}']`);
+            if (!!marketSelector) {
+                marketSelector.click();
+            }
 
             // Select the date range
-            document.querySelector(`ul.market-chart--period li a[class*='${DATE_RANGE}']`).click();
+            const dateRangeSelector = document.querySelector(`ul.market-chart--period li a[class*='${DATE_RANGE}']`);
+            if (!!dateRangeSelector) {
+                dateRangeSelector.click();
+            }
         }, 0);
     });
 }());
