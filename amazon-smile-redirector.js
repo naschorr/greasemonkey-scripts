3dc4d2af8620b9fd8e42065c6fa396b9bd105403
Nick Schorr <naschorr@gmail.com>
2021-06-29 15:40:12 -0400

    Moved URL parsing logic into UserScript meta block via @include and @exclude

--- amazon-smile-redirector.js
+++ amazon-smile-redirector.js
@@ -1,10 +1,12 @@
 // ==UserScript==
 // @name         Amazon Smile Redirector
 // @namespace    https://github.com/naschorr/userscripts
-// @version      0.2
+// @version      0.3
 // @description  Redirects regular Amazon URLs to their equivalent Amazon Smile domain
 // @author       You
-// @match        https://*.amazon.com/*
+// @include      https://*amazon.com/*
+// @exclude      https://*aws*amazon.com/*
+// @exclude      https://*smile*amazon.com/*
 // @icon         https://www.google.com/s2/favicons?domain=amazon.com
 // @grant        none
 // ==/UserScript==
@@ -14,15 +16,6 @@
 
     // Get a useful URL object, versus the USVString returned by location.href
     const url = new URL(location.href);
-
-    /*
-        Don't redirect if we're already at the smile subdomain. Similarly don't redirect
-        if we're at an AWS subdomain.
-    */
-    const hostname = url.hostname.toLowerCase();
-    if (hostname.includes('smile') || hostname.includes('aws')) {
-        return;
-    }
 
     // Update the hostname
     url.hostname = 'smile.amazon.com';
