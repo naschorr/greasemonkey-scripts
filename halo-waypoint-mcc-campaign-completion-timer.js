e17ea41f85799fb862db04efaa818794a9a58b61
Nick Schorr <naschorr@gmail.com>
2022-02-13 20:37:08 -0500

    Added simple time aggregator for halo waypoint's MCC campaign completion times

---
+++ halo-waypoint-mcc-campaign-completion-timer.js
@@ -0,0 +1,46 @@
+// ==UserScript==
+// @name         Halo Waypoint MCC Campaign Completion Time Tracker
+// @namespace    https://github.com/naschorr/userscripts
+// @version      0.1
+// @description  Adds up and displays the campaign completion times in Halo Waypoint's MCC Service Record
+// @author       https://github.com/naschorr
+// @match        https://*halowaypoint.com/halo-the-master-chief-collection/*
+// @icon         https://www.google.com/s2/favicons?domain=halowaypoint.com
+// @grant        none
+// ==/UserScript==
+
+function calculateCompletionDateString(nodelist) {
+    const seconds = Array.from(nodelist).reduce(
+        (previousValue, element) => {
+          const time = parseFloat(element.textContent) || 0;
+          return previousValue + time;
+        },
+        0
+    );
+  
+    return new Date(seconds * 1000).toISOString().substr(11, 8);
+}
+
+function updateCompletionTimes() {
+    const singlePlayerCompletionNodes = document.querySelectorAll('.campaign-tables table[aria-label*="Single Player"] .labels+tbody tr td:last-child');
+    const coopCompletionNodes = document.querySelectorAll('.campaign-tables table[aria-label*="Co-Op"] .labels+tbody tr td:last-child');
+
+    const singlePlayerCompletionDateString = calculateCompletionDateString(singlePlayerCompletionNodes);
+    const coopCompletionDateString = calculateCompletionDateString(coopCompletionNodes);
+    
+    const tables = document.querySelectorAll('.campaign-tables .title')
+    tables[0].textContent = `Single Player (${singlePlayerCompletionDateString})`
+    tables[1].textContent = `Co-Op (${coopCompletionDateString})`
+}
+
+(function() {
+    'use strict';
+    console.log("checking!");
+    const checkInterval = setInterval(() => {
+        if (document.querySelector('.campaign-tables table[aria-label*="Single Player"] .labels+tbody') != null) {
+            console.log("Loaded!");
+            updateCompletionTimes();
+            clearInterval(checkInterval);
+        }
+    }, 250);
+})();
